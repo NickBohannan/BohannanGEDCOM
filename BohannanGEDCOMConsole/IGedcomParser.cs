7afc92a48e3dae82a4e0cb31ad29ae70213952a1
NickBohannan <34175376+NickBohannan@users.noreply.github.com>
2023-07-16 16:02:03 -0500

    Update IGedcomParser.cs indenting

--- BohannanGEDCOMConsole/IGedcomParser.cs
+++ BohannanGEDCOMConsole/IGedcomParser.cs
@@ -6,7 +6,7 @@ namespace BohannanGEDCOMConsole
 	public interface IGedcomParser
 	{
 		public List<CsvEntry> PopulateCSV(List<GeneGenie.Gedcom.GedcomIndividualRecord> familyList);
-        public void CreateCSV(List<GeneGenie.Gedcom.GedcomIndividualRecord> familyList);
+        	public void CreateCSV(List<GeneGenie.Gedcom.GedcomIndividualRecord> familyList);
 		public List<GeneGenie.Gedcom.GedcomIndividualRecord> ParseGedcom(string[] args);
     }
 }
